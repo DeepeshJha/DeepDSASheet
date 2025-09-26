d838b82b2550baa670ef82acd6914937449a8a3f
DeepeshJha <deepeshjha17@gmail.com>
2025-09-26 18:06:50 +0530

    Basic recursion continued

--- basicrecursion.js
+++ basicrecursion.js
@@ -38,3 +38,67 @@ function printNumberNto1Backtracking(i,n) {
     process.stdout.write(`${i}\n`)
 }
 printNumberNto1Backtracking(1,5)
+
+console.log("\nSum of first n numbers using functional recursion")
+function sumOfFirstNnumbersFunctional(n) {
+    if(n==0) return 0;
+    return n + sumOfFirstNnumbersFunctional(n-1);
+}
+console.log('sumOfFirstNnumbersFunctional => ', sumOfFirstNnumbersFunctional(6))
+
+console.log("\nSum of first n numbers using parameterised recursion")
+function sumOfFirstNnumbersParameterised(i,n) {
+    if(i<=1) {
+        process.stdout.write(`${n}`);
+        return;
+    }
+    sumOfFirstNnumbersParameterised(i-1, n + i-1)
+}
+sumOfFirstNnumbersParameterised(6,6);
+
+console.log("\nFactorial of N numbers")
+function factorialOfNnumbers(n) {
+    if(n<=1) return 1;
+    return n*factorialOfNnumbers(n-1)
+}
+console.log(factorialOfNnumbers(5))
+
+console.log("\nReverse an array - Two pointers")
+function reverseArrayTwoPointers(arr) {
+    for(let i=0; i<arr.length/2; i++) {
+        let temp = arr[i];
+        arr[i] = arr[arr.length-i-1];
+        arr[arr.length-i-1] = temp;
+    }
+}
+reverseArrayTwoPointers([2,3,4,7,5,1,6])
+
+console.log("\nReverse an array - Recursion")
+function reverseArrayRecursion(l,r,arr) {
+    if(l>=r) return arr;
+    let temp = arr[l];
+    arr[l] = arr[r];
+    arr[r] = temp;
+    return reverseArrayRecursion(l+1,r-1,arr)
+}
+let arr = [5,6,7,3,2,1,9]
+console.log('reverseArrayRecursion, ', reverseArrayRecursion(0,arr.length-1, arr))
+
+console.log("\nCheck Palindrome - Recursion")
+function checkPalindrome(l,r,arr) {
+    if(l>=r) {
+        return true;
+    }
+    if(arr[l] !== arr[r]) return false;
+    return checkPalindrome(l+1,r-1,arr)
+}
+let stringVal = 'MALAYALAM'
+let arrVal = stringVal.split('')
+console.log('checkPalindrome, ',checkPalindrome(0,arrVal.length-1, arrVal))
+
+// console.log("\nFibinacci series - Recursion")
+// function fibinacciSeriesTillN(n) {
+//     if(n<=1) return n;
+//     l = n-1;
+//     s = n-2; 
+// }
